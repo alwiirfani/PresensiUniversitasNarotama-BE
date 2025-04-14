099dba0ca44e2d2fb44ce527f56fada44622e2c7
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2025-04-14 14:31:23 +0700

    [CHANGE] add serverlessHttp to deploying needed

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import { logger, loggerInfo } from "./configs/logger.js";
 import { adminRoute } from "./routes/admin-route.js";
 import { publicRoute } from "./routes/public-route.js";
+import serverless from "serverless-http";
 
 const app = express();
 app.use(cors());
@@ -14,6 +15,10 @@ app.use(loggerInfo);
 
 app.use(adminRoute, publicRoute);
 
-app.listen(process.env.SERVER_PORT, () => {
-  logger.info(`Server running on port ${process.env.SERVER_PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(process.env.SERVER_PORT, () => {
+    logger.info(`Server running on port ${process.env.SERVER_PORT}`);
+  });
+}
+
+export const handler = serverless(app);
